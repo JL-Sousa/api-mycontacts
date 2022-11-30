122b84559eea74ef319e4b0e76bdc4d421bd61b5
Jonyelson Lopes <jonyelson20@gmail.com>
2022-11-29 22:28:28 -0300

    rota cadastro de contatos

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
@@ -18,8 +19,26 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
-    // Cria novo registro
+  async store(request, response) {
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This e-mail is already been taken' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   update() {
